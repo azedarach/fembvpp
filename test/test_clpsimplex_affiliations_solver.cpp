4be5acdb4065badfc0a0724100406fabbba70553
Dylan Harries <Dylan.Harries@csiro.au>
2019-06-20 16:46:03 +1000

    Add additional trivial tests

--- test/test_clpsimplex_affiliations_solver.cpp
+++ test/test_clpsimplex_affiliations_solver.cpp
@@ -23,4 +23,49 @@ TEST_CASE("test basic construction", "[ClpSimplex_affiliations_solver]")
 
       CHECK(solver.get_max_tv_norm() == max_tv_norm);
    }
+
+   SECTION("number of components set correctly")
+   {
+      const int n_components = 6;
+      const int n_elements = 10;
+      const int n_samples = 30;
+      const double max_tv_norm = 0;
+
+      Eigen::MatrixXd G(Eigen::MatrixXd::Zero(n_components, n_samples));
+      Eigen::MatrixXd V(Eigen::MatrixXd::Zero(n_elements, n_samples));
+
+      ClpSimplex_affiliations_solver solver(G, V, max_tv_norm);
+
+      CHECK(solver.get_n_components() == n_components);
+   }
+
+   SECTION("number of elements set correctly")
+   {
+      const int n_components = 6;
+      const int n_elements = 10;
+      const int n_samples = 30;
+      const double max_tv_norm = 0;
+
+      Eigen::MatrixXd G(Eigen::MatrixXd::Zero(n_components, n_samples));
+      Eigen::MatrixXd V(Eigen::MatrixXd::Zero(n_elements, n_samples));
+
+      ClpSimplex_affiliations_solver solver(G, V, max_tv_norm);
+
+      CHECK(solver.get_n_elements() == n_elements);
+   }
+
+   SECTION("number of samples set correctly")
+   {
+      const int n_components = 6;
+      const int n_elements = 10;
+      const int n_samples = 30;
+      const double max_tv_norm = 0;
+
+      Eigen::MatrixXd G(Eigen::MatrixXd::Zero(n_components, n_samples));
+      Eigen::MatrixXd V(Eigen::MatrixXd::Zero(n_elements, n_samples));
+
+      ClpSimplex_affiliations_solver solver(G, V, max_tv_norm);
+
+      CHECK(solver.get_n_samples() == n_samples);
+   }
 }
